e903af33a1032f6fad03146f39fa2dd1056bbbd1
morg0913 <34302267+morg0913@users.noreply.github.com>
2020-04-20 01:22:57 +0300

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,52 +1,59 @@
-import React from 'react';
-import SearchBar from './SearchBar';
-import youtube from '../apis/youtube';
-import VideoList from './VideoList';
-import VideoDetail from './VideoDetail';
+import React from "react";
+import SearchBar from "./SearchBar";
+import youtube from "../apis/youtube";
+import VideoList from "./VideoList";
+import VideoDetail from "./VideoDetail";
 
 class App extends React.Component {
-    state = { videos: [], selectedVideo: null };
+    state = { videos: [], selectedVideo: null, error: "" };
 
     componentDidMount() {
-        this.onTermSubmit('buildings');
+        this.onTermSubmit("buildings");
     }
 
     onTermSubmit = async term => {
-      const response = await youtube.get('/search', {
-            params:{
-            q: term
-            }
+        try {
+            const response = await youtube.get("/search", {
+                params: {
+                    q: term
+                }
+            });
 
-      });
-        this.setState({
-            videos: response.data.items,
-            selectedVideo: response.data.items[0]
-        });
+            this.setState({
+                videos: response.data.items,
+                selectedVideo: response.data.items[0]
+            });
+        } catch (err) {
+            this.setState({
+                error: err.message
+            });
+        }
     };
 
-    onVideoSelect = (video) => {
+    onVideoSelect = video => {
         this.setState({ selectedVideo: video });
     };
 
     render() {
         return (
             <div className="ui container">
+                {this.state.error && <h2>Error: {this.state.error}</h2>}
                 <SearchBar onFormSubmit={this.onTermSubmit} />
                 <div className="ui grid">
                     <div className="ui row">
-                        <div className=" eleven wide column">
+                        <div className="eleven wide column">
                             <VideoDetail video={this.state.selectedVideo} />
                         </div>
                         <div className="five wide column">
-                        <VideoList
-                           onVideoSelect={this.onVideoSelect}
-                           videos={this.state.videos}
+                            <VideoList
+                                onVideoSelect={this.onVideoSelect}
+                                videos={this.state.videos}
                             />
-                            </div>
                         </div>
                     </div>
+                </div>
             </div>
-            );
+        );
     }
 }
 
